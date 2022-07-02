600ad83fad835f7f6f552f5d0b32b4ec94c24344
tetratoon <gerhardsprung@gmail.com>
2022-07-02 21:09:56 +0200

    init

---
+++ dmt_HeadController.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DMT_Icon.DMT_Slice
+{
+    public class dmt_HeadController : MonoBehaviour
+    {
+
+        //DEBUG
+        public bool debug = true;
+        public Transform DebugCenter;
+
+        public Vector3 DebugInputPoint;
+        // end DEBUG
+
+        //public float headScale = 0.05f;
+        private GameObject headObject;
+        private Vector3 headPos;
+
+        //die drei Slice-objekte
+        private dmt_Slice sliceX, sliceY, sliceZ;
+        private dmt_Slice[] sliceList;
+
+        //Die Layer 
+        public string[] LayerNames;
+        private int activeLayer = 0;
+
+        void Start()
+        {
+            // Dummy im unteren Eck des Kopfes zur Positionierung 
+            if ((headObject = GameObject.FindWithTag("HEAD")) == null) throw new Exception("no Headobject found");
+            headPos = headObject.transform.position;
+            sliceX = GameObject.FindWithTag("sliceX").GetComponent<dmt_Slice>();
+            sliceX.axis = dmt_Slice.AXIS.X;
+            sliceY = GameObject.FindWithTag("sliceY").GetComponent<dmt_Slice>();
+            sliceY.axis = dmt_Slice.AXIS.Y;
+            sliceZ = GameObject.FindWithTag("sliceZ").GetComponent<dmt_Slice>();
+            sliceZ.axis = dmt_Slice.AXIS.Z;
+
+            sliceList = new[] {sliceX, sliceY, sliceZ};
+
+            for (int i = 0; i < LayerNames.Length; i++)
+            {
+                foreach (var slice in sliceList)
+                {
+                    slice.addLayer(LayerNames[i]);
+                }
+            }
+            
+            //TODO testen und aktivieren
+            
+            
+            // foreach (var slice in sliceList)
+            // {
+            //     slice.setLayerActive(activeLayer);
+            // }
+            
+            
+        }
+
+
+        void Update()
+        {
+            DebugInputPoint = DebugCenter.position;
+            var v = CalculateRelativePosition(DebugInputPoint);
+
+            foreach (var slice in sliceList)
+            {
+                slice.showSlice(v, debug);
+            }
+
+        }
+
+        private Vector3 CalculateRelativePosition(Vector3 point)
+        {
+            var t = DebugInputPoint - headPos;
+            t.x /= headObject.transform.localScale.x;
+            t.y /= headObject.transform.localScale.y;
+            t.z /= headObject.transform.localScale.z;
+            return t / 10f; //planes sind 10 Einheiten groß
+        }
+    }
+}
+
---
+++ dmt_Slice.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using DMT_Icon.DMT_Slice;
+using TMPro;
+using UnityEngine;
+
+namespace DMT_Icon.DMT_Slice
+{
+    public class dmt_Slice : MonoBehaviour
+    {
+        public TMP_Text text;
+        public Transform sliceObject;
+        private Vector3 Origin; //nullpunkt 
+        private float Value = 0f;
+
+        // Anzahl der Bilder
+        private int steps = 0;
+
+        private List<dmt_Layer> layerList= new List<dmt_Layer>();
+        public int activeLayer = 0;
+
+
+
+        public AXIS axis;
+
+        public enum AXIS
+        {
+            X,
+            Y,
+            Z
+        }
+
+        void Start()
+        {
+            sliceObject = this.transform.Find("SlicePlane");
+            Origin = sliceObject.localPosition;
+
+
+        }
+
+        public int addLayer(string layerName)
+        {
+           
+            dmt_Layer tLayer=  this.gameObject.AddComponent<dmt_Layer>();
+            tLayer.set(layerName, axis);
+            layerList.Add(tLayer);
+           
+            steps = layerList[layerList.Count-1].steps;
+            return steps;
+
+        }
+
+        public int setLayerActive(int layerToActivate)
+        {
+            activeLayer = layerToActivate;
+            return layerList[activeLayer].steps;
+        }
+
+        void Update()
+        {
+
+        }
+
+        public void showSlice(Vector3 Val, bool debug = false)
+        {
+            switch (axis)
+            {
+                case AXIS.X:
+                    Value = Val.x;
+                    Val = new Vector3(Val.x, 0f, 0f);
+                    break;
+                case AXIS.Y:
+                    Value = Val.y;
+                    Val = new Vector3(0f, Val.y, 0f);
+                    break;
+                case AXIS.Z:
+                    Value = Val.z;
+                    Val = new Vector3(0f, Val.z, 0f);
+                    break;
+                default: break;
+            }
+
+            sliceObject.localPosition = Origin + Val * 10f; //planes sind 10 Einheiten groß
+
+            var i = Mathf.RoundToInt(Value * (steps - 1));
+            if (debug) text.text = Value.ToString() + "\n" + i.ToString();
+            sliceObject.GetComponent<Renderer>().material.mainTexture = layerList[0].Images[i];
+
+        }
+    }
+}
